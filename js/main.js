fae5f1e3eb6a464b0c0f1f8ed4683443d5774f4c
Francisco Rodrigues <francisco-charlie@hotmail.com>
2020-05-07 01:22:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,24 +16,24 @@ var bible_version = localStorage.getItem("bible_version");
 
 switch (bible_version) {
     case 'pt_acf':
-        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/sermao/master/db/books/json/pt_acf.json'
+        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/biblie/master/db/books/json/pt_acf.json'
         break;
     case 'pt_nvi':
-        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/sermao/master/db/books/json/pt_nvi.json'
+        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/biblie/master/db/books/json/pt_nvi.json'
         break;
     case 'pt_aa':
-        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/sermao/master/db/books/json/pt_aa.json'
+        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/biblie/master/db/books/json/pt_aa.json'
         break;
 
     case 'en_bbe':
-        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/sermao/master/db/books/json/en_bbe.json'
+        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/biblie/master/db/books/json/en_bbe.json'
         break;
     case 'en_kjv':
-        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/sermao/master/db/books/json/en_kjv.json'
+        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/biblie/master/db/books/json/en_kjv.json'
         break;
 
     default:
-        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/sermao/master/db/books/json/pt_aa.json'
+        bible_url = 'https://raw.githubusercontent.com/rodriguesfas/biblie/master/db/books/json/pt_aa.json'
         break;
 }
 
